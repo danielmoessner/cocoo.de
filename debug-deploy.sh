8b8379472d7ef9e70eff26e263fdf4338c0ac6ce
danielmoessner <github@moesibert.de>
2021-04-16 12:25:55 +0200

    updates

--- debug-deploy.sh
+++ debug-deploy.sh
@@ -3,7 +3,6 @@ git pull
 #tmp/venv/bin/pip install -r requirements.txt
 tmp/venv/bin/python manage.py migrate
 ./permissions.sh
-npm install
 npm run css
 npm run js
 tmp/venv/bin/python manage.py collectstatic --clear --noinput
