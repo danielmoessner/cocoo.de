d54917e65018be68bbe19aa66187081594c93181
Daniel Mössner <github@moesibert.de>
2023-12-30 19:57:08 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,4 +1,9 @@
+# backup db
 TODAY=`date +'%Y-%m-%d'`
 FILENAME="tmp/backup/${TODAY}.sqlite3"
 cp tmp/db.sqlite3 ${FILENAME}
 echo $FILENAME
+
+# delete old backups
+PREVIOUS=`date -d '1 year ago' +%Y`
+find tmp/backup/ -name "${PREVIOUS}-*" -exec rm {} \;
