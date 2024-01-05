cd391f5014c75f925b1a489a5b3e26dd719f2f12
Daniel Mössner <github@moesibert.de>
2024-01-05 11:12:39 +0100

    update

--- push-db.sh
+++ push-db.sh
@@ -1,1 +1,2 @@
-scp ./tmp/db.sqlite3 root@$1:/home/cocoo.de/tmp/db.sqlite3
+# scp ./tmp/db.sqlite3 root@$1:/home/cocoo.de/tmp/db.sqlite3
+# scp ./tmp/secrets.json root@$1:/home/cocoo.de/tmp/secrets.json
