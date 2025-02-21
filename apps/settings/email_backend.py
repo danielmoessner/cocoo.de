d3310347cbbb03e9b02084e3389be7d378587060
attikoszto <131347235+attikoszto@users.noreply.github.com>
2025-02-21 13:59:26 +0100

    change

--- apps/settings/email_backend.py
+++ apps/settings/email_backend.py
@@ -8,13 +8,15 @@ from .models import General
 class DbEmailBackend(EmailBackend):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        
         try:
             general = General.get_solo()
-            self.host = general.email_host or self.host
-            self.port = general.email_port or self.port
-            self.username = general.email_host_user or self.username
-            self.password = general.email_host_password or self.password
-            self.use_tls = general.email_use_tls
-            settings.DEFAULT_FROM_EMAIL = general.default_from_email or settings.DEFAULT_FROM_EMAIL
         except (OperationalError, ProgrammingError):
-            pass
+            return  
+        
+        self.host = general.email_host or self.host
+        self.port = general.email_port or self.port
+        self.username = general.email_host_user or self.username
+        self.password = general.email_host_password or self.password
+        self.use_tls = general.email_use_tls
+        settings.DEFAULT_FROM_EMAIL = general.default_from_email or settings.DEFAULT_FROM_EMAIL
